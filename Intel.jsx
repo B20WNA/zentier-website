06d9f971dfda0cb5ef3248202211e8c554b5e8ba
B20WNA <anthony@arkbridgelife.com>
2026-04-29 11:18:12 +0100

    ab

--- Intel.jsx
+++
@@ -1,5 +0,0 @@
-export function scrollToSection(e, id) {
-  e.preventDefault()
-  const target = document.getElementById(id)
-  if (target) target.scrollIntoView({ behavior: 'smooth', block: 'start' })
-}
